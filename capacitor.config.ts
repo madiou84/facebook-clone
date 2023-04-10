342ffbf2f4fe08cc34a0737a034005e5a5206284
Madiou BAH <mmadioubah@gmail.com>
2023-04-10 20:19:20 +0200

    add capacotor on project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fbclone.app',
+  appName: 'facebook-clone',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
